f901091f61f632207d29e7d29c7a47640089ae12
melika <melikmkj4@gmail.com>
2025-03-25 10:19:08 +0330

    Added user.h

--- mini project/user.h
+++ mini project/user.h
@@ -9,9 +9,12 @@ class User
 private:
     string username; 
     string password;
-    
-public:
 
+public:
+    User(string uname, string pass);
+    virtual void displayRole() = 0;
+    bool logIn(string uname, string pass);
+    void logOut();
 }; 
 
 #endif
